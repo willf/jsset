92bd1659d18cc2e139e08c72b5d8146191e29c38
Will Fitzgerald <will@wordnik.com>
2012-04-06 17:17:22 -0400

    removed files that shouldn\'t be here

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# unbind if bound
-ps -awx | grep "ssh -f -N -L"  | grep "8012" | awk '{print $1}' | xargs  kill
-# bind
-ssh -f -N -L 8012:localhost:8012 ec2-50-18-237-212.us-west-1.compute.amazonaws.com
-# 
-open public/fretboard.html
